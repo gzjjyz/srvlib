8103892cd7c0edaed006685123451b90c2492004
jidibinlin <2694273649@qq.com>
2023-10-12 15:21:58 +0800

    refact: refact natsq and write necessary comment

--- mq/natsq/nats.go
+++ mq/natsq/nats.go
@@ -7,9 +7,11 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
-// NatsConn: manage nats connection and subscriptions.
+// NatsConn: manage nats connection and Subscriptions.
+// Don`t use Subscription.UnSubScribe function instead use NatsConn.UnSubScribe except you known what are you doing, for the reason it would confuse NatsConn
+// Don`t use Subscription.Drain function instead use NatsConn.DrainSubscription except you known what are you doing, for the reason it would confuse NatsConn
 type NatsConn struct {
-	*nats.Conn
+	rawConn       *nats.Conn
 	subscriptions sync.Map
 }
 
@@ -44,11 +46,11 @@ func Init(url string, opts ...nats.Option) error {
 	}
 
 	conn = &NatsConn{
-		Conn:          rawNatsConn,
+		rawConn:       rawNatsConn,
 		subscriptions: sync.Map{},
 	}
 
-	rawJs, err := conn.Conn.JetStream()
+	rawJs, err := conn.rawConn.JetStream()
 	if err != nil {
 		return err
 	}
@@ -64,21 +66,40 @@ func GetConn() *NatsConn {
 	return conn
 }
 
+// Notice!!!!: don`t use rawConn`s Subscribe group functions
+// for the reason if would confuse NatsConn
+func (c *NatsConn) RawNatsConn() *nats.Conn {
+	return c.rawConn
+}
+
+// Notice!!!: don`t use rawConn`s JetStream() function Use NatsConn`s JetStream() function instead
+// for the reason it would confuse NatsConn
 func (c *NatsConn) JetStream() nats.JetStreamContext {
 	return js
 }
 
-// generally you don`t need to use this method, the "raw nats conn" will manage all subscriptions.
-// only when you have further manipulation on subscription you should registe it.
-func (c *NatsConn) RegisterSubscribtion(topic string, subObj *nats.Subscription) error {
-	_, ok := c.subscriptions.Load(topic)
-
-	if ok {
-		return ErrTopicAlreadyRegistered
+func (c *NatsConn) UnSubscribe(topic string) error {
+	foo, ok := c.subscriptions.Load(topic)
+	if !ok {
+		return ErrTopicNotRegistered
 	}
 
-	c.subscriptions.Store(topic, subObj)
+	sub, ok := foo.(*nats.Subscription)
+	if !ok {
+		return ErrTopicNotRegistered
+	}
+
+	err := sub.Drain()
+	if err != nil {
+		return err
+	}
+
+	c.subscriptions.Delete(topic)
 	return nil
+}
+
+func (c *NatsConn) DrainSubscription(topic string) error {
+	return c.UnSubscribe(topic)
 }
 
 func (c *NatsConn) GetRegistedSub(topic string) *nats.Subscription {
@@ -101,30 +122,103 @@ func (c *NatsConn) Subscribe(subj string, cb nats.MsgHandler) (sub *nats.Subscri
 		return nil, ErrTopicAlreadyRegistered
 	}
 
-	sub, err = c.Conn.Subscribe(subj, cb)
+	sub, err = c.rawConn.Subscribe(subj, cb)
 	if err != nil {
 		return nil, err
 	}
 
-	err = c.RegisterSubscribtion(subj, sub)
+	c.subscriptions.Store(subj, sub)
+	return
+}
+
+func (c *NatsConn) SubscribeSync(subj string) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = c.rawConn.SubscribeSync(subj)
+	if err != nil {
+		return nil, err
+	}
+
+	c.subscriptions.Store(subj, sub)
+	return
+}
+
+func (c *NatsConn) QueueSubscribe(subj, queue string, cb nats.MsgHandler) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = c.rawConn.QueueSubscribe(subj, queue, cb)
+	if err != nil {
+		return nil, err
+	}
+
+	c.subscriptions.Store(subj, sub)
 	return sub, err
 }
 
-// NOTICE: Gnerally conn will manage all subscriptions, so you don`t need to Unsubscribe manually.
-// in special case, you can use this method to Unsubscribe as you whish.
-func (c *NatsConn) UnSubscribeRegsitedSub(topic string) error {
-	subObj := c.GetRegistedSub(topic)
-	if subObj == nil {
-		return ErrTopicNotRegistered
+func (c *NatsConn) ChanSubscribe(subj string, ch chan *nats.Msg) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
 	}
 
-	err := subObj.Drain()
+	sub, err = c.rawConn.ChanSubscribe(subj, ch)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	c.subscriptions.Delete(topic)
-	return nil
+	c.subscriptions.Store(subj, sub)
+	return
+}
+
+func (c *NatsConn) QueueSubscribeSync(subj, queue string) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = c.rawConn.QueueSubscribeSync(subj, queue)
+	if err != nil {
+		return nil, err
+	}
+
+	c.subscriptions.Store(subj, sub)
+	return
+}
+
+func (c *NatsConn) QueueSubscribeSyncWithChan(subj, queue string, ch chan *nats.Msg) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = c.rawConn.QueueSubscribeSyncWithChan(subj, queue, ch)
+	if err != nil {
+		return nil, err
+	}
+
+	c.subscriptions.Store(subj, sub)
+	return
+}
+
+func (c *NatsConn) ChanQueueSubscribe(subj, queue string, ch chan *nats.Msg) (sub *nats.Subscription, err error) {
+	_, ok := c.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = c.rawConn.ChanQueueSubscribe(subj, queue, ch)
+	if err != nil {
+		return nil, err
+	}
+
+	c.subscriptions.Store(subj, sub)
+	return
 }
 
 func (c *NatsConn) DrainAllSubscription(errCb DrainAllSubscriptionsErrorCb) {
@@ -142,19 +236,18 @@ func (c *NatsConn) DrainAllSubscription(errCb DrainAllSubscriptionsErrorCb) {
 	})
 }
 
-// Close will wait for all msg processed and Unsubscribe all  conn/JetStream`s subscriptions.
 func (c *NatsConn) Close() error {
 	if conn == nil {
 		return nil
 	}
 
-	return c.Drain()
+	return c.rawConn.Drain()
 }
 
 // ForceClose is unsafe close, it will close connection immediately without process left msg
 // and  unscript conn/JetStream`s subscriptions.
 func (c *NatsConn) ForceClose() {
-	c.Close()
+	c.rawConn.Close()
 }
 
 type JetStreamContext struct {
@@ -172,6 +265,96 @@ func (js *JetStreamContext) Subscribe(topic string, cb nats.MsgHandler, opts ...
 		return nil, err
 	}
 
-	err = conn.RegisterSubscribtion(topic, sub)
+	conn.subscriptions.Store(topic, sub)
+	return
+}
+
+func (js *JetStreamContext) SubscribeSync(subj string, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.SubscribeSync(subj, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(subj, sub)
+	return
+}
+
+func (js JetStreamContext) QueueSubscribe(topic, queue string, cb nats.MsgHandler, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(topic)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.QueueSubscribe(topic, queue, cb)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(topic, sub)
+	return
+}
+
+func (js JetStreamContext) QueueSubscribeSync(subj, queue string, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(subj)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.QueueSubscribeSync(subj, queue, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(subj, sub)
+	return
+}
+
+func (js *JetStreamContext) ChanSubscribe(topic string, ch chan *nats.Msg, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(topic)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.ChanSubscribe(topic, ch)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(topic, sub)
+	return
+}
+
+func (js JetStreamContext) ChanQueueSubscribe(topic, queue string, ch chan *nats.Msg, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(topic)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.ChanQueueSubscribe(topic, queue, ch)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(topic, sub)
 	return sub, err
+}
+
+func (js JetStreamContext) PullSubscribe(subject, durable string, opts ...nats.SubOpt) (sub *nats.Subscription, err error) {
+	_, ok := conn.subscriptions.Load(subject)
+	if ok {
+		return nil, ErrTopicAlreadyRegistered
+	}
+
+	sub, err = js.JetStreamContext.PullSubscribe(subject, durable)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.subscriptions.Store(subject, sub)
+	return
 }
