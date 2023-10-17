f02db6536de9249ff3cbd8970f2d4b2215c06404
zhangjianjun <1005777562@qq.com>
2023-10-17 11:38:03 +0800

    feat: MakeOrGetSpecElemMu 并发问题

--- lock/lock.go
+++ lock/lock.go
@@ -65,38 +65,46 @@ func (m *WithUsageMu) RUnlock() {
 }
 
 type MulElemMuFactory struct {
-	elemMuMap map[interface{}]*WithUsageMu
+	elemMuMap sync.Map
 	opMapMu   sync.Mutex
 }
 
 func NewMulElemMuFactory() *MulElemMuFactory {
-	return &MulElemMuFactory{
-		elemMuMap: map[interface{}]*WithUsageMu{},
-	}
+	return &MulElemMuFactory{}
 }
 
 func (m *MulElemMuFactory) MakeOrGetSpecElemMu(elem interface{}) *WithUsageMu {
-	mu, ok := m.elemMuMap[elem]
+	mu, ok := m.elemMuMap.Load(elem)
 	if !ok {
 		mu = NewWithUsageMu(
 			&m.opMapMu,
 			func() {
 				// save this lock while any thread used the handler lock
-				if _, ok := m.elemMuMap[elem]; ok {
+				if _, ok := m.elemMuMap.Load(elem); ok {
 					return
 				}
-				m.elemMuMap[elem] = mu
+				m.elemMuMap.Store(elem, mu)
 			},
 			func() {
 				// remove this lock from map if this lock might have no owner
-				if mu.UsageNum > 0 {
+				if mu == nil {
+					m.elemMuMap.Delete(elem)
 					return
 				}
-				delete(m.elemMuMap, elem)
+				lmu, ok := mu.(*WithUsageMu)
+				if !ok {
+					m.elemMuMap.Delete(elem)
+					return
+				}
+				// remove this lock from map if this lock might have no owner
+				if lmu.UsageNum > 0 {
+					return
+				}
+				m.elemMuMap.Delete(elem)
 			},
 		)
 
-		m.elemMuMap[elem] = mu
+		m.elemMuMap.Store(elem, mu)
 	}
-	return mu
+	return mu.(*WithUsageMu)
 }
